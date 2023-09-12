7b77c4d4bb4f6379eb6a2544582a017996093f82
Yarik123121 <144149159+Yarik123121@users.noreply.github.com>
2023-09-12 19:47:39 +0300

    Update ConsoleApplication39.cpp

--- ConsoleApplication39/ConsoleApplication39.cpp
+++ ConsoleApplication39/ConsoleApplication39.cpp
@@ -16,6 +16,9 @@ int main()
        std::cout << "Hello World!\n";
         std::cout << "Hello World22222244!\n";
        std::cout << "Hello World!\n";
+
+    std::cout << "Hello World!\n";
+        
         std::cout << "Hello World22222244!\n";
        std::cout << "Hello World!\n";
         std::cout << "Hello World22222244!\n";
